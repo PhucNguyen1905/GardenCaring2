e9fef56e1a9181894a3d7cf2aa8f4e14c58fb710
Nguyễn Trọng Phúc <ngtrongphuc1905@gmail.com>
2022-03-27 15:16:55 +0700

    Add env and sql function

---
+++ models/deviceModel.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql");
+
+//Connection pool
+const connection = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
